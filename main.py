db14a21f864b70ac90ca530188a4ac63d7e2ffd5
Fishiest <tntat444@gmail.com>
2021-10-07 18:27:10 -0400

    rock paper scissors idk

--- main.py
+++ main.py
@@ -2,76 +2,68 @@
 #rock paper scissors because i need to learn how to spell scissors
 
 from random import randint
-print("I was thinking pulling a spamton, but not everyone will know who he is, so rock, paper, or scissors?")
+
+
+
 def rpschoose():
     global rps
-    if input() == "rock":
+    if input() == "rock" or "Rock" or "R" or "r":
         rps = 1
     else:
-        if input() == "paper":
+        if input() == "paper" or "Paper" or "P" or "p":
             rps = 2
         else:
-            if input() == "scissors":
+            if input() == "scissors" or "Scissors" or "S" or "s":
                 rps = 3
-                if input() == "Rock":
-                    rps = 1
-                else:
-                    if input() == "Paper":
-                        rps = 2
-                    else:
-                        if input() == "Scissors":
-                            rps = 3
-                            if input() == "r":
-                                rps = 1
-                            else:
-                                if input() == "p":
-                                    rps = 2
-                                else:
-                                    if input() == "s":
-                                        rps = 3
-                                        if input() == "R":
-                                            rps = 1
-                                        else:
-                                            if input() == "P":
-                                                rps = 2
-                                            else:
-                                                if input() == "S":
-                                                    rps = 3
             else:
-                print ("You didn't type rock, paper, or scissors, restarting...")
+                print ("You didn't type rock, paper, or scissors, (or mispelled it-) restarting...")
                 rpschoose()
-comprps = randint(1,3)
-rpschoose()
-#Saying what the computer chose.
-if comprps == 1:
-    print("The computer chose rock!")
-else:
-    if comprps == 2:
-        print("The computer chose paper!")
+def main():
+    print("I was thinking pulling a spamton, but not everyone will know who he is, so rock, paper, or scissors?")
+    rpschoose()
+    comprps = randint(1,3)
+    rpschoose()
+    #Saying what the computer chose.
+    if comprps == 1:
+        print("The computer chose rock!")
     else:
-        if comprps == 3:
-            print("The computer chose scissors!")
-#now for the mess of if/thens...
-if rps == comprps:
-    print("Its a tie!")
-else:
-    if rps == 1:
         if comprps == 2:
-            print ("The computer won!")
+            print("The computer chose paper!")
         else:
             if comprps == 3:
-                print ("You won!")
+                print("The computer chose scissors!")
+    #now for the mess of if/thens...
+    if rps == comprps:
+        print("Its a tie!")
     else:
-        if rps == 2:
-            if comprps == 1:
-                print ("You won!")
+        if rps == 1:
+            if comprps == 2:
+                print ("The computer won!")
             else:
                 if comprps == 3:
-                    print ("The computer won!")
-        else:
-            if rps == 3:
-                if comprps == 1:
-                    print ("The computer won!")
-            else:
-                if comprps == 2:
                     print ("You won!")
+        else:
+            if rps == 2:
+                if comprps == 1:
+                    print ("You won!")
+                else:
+                    if comprps == 3:
+                        print ("The computer won!")
+            else:
+                if rps == 3:
+                    if comprps == 1:
+                        print ("The computer won!")
+                else:
+                    if comprps == 2:
+                        print ("You won!")
+    print ("Would you like a rematch?")
+    (input.lower())
+    reanswer = input()
+    if "y" in reanswer:
+        main()
+    else:
+        exit()
+        print("Alright sounds good!")
+    print("Alright sounds good!")
+
+#nice.

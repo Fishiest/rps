409107aa4ae6d531ed9c88a5fb91e4738845a192
Fishiest <tntat444@gmail.com>
2021-11-16 14:55:12 -0500

    rock paper scissors but too long

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ listOfChoice = {"r", "p", "sc", "l", "sp"}
 import random
 
 rps = "tehe this is just so the code doesn't break :)"
-def choice():
-    global rps
+while True:
+    #getting a choice
     humanChoice = input("What will your choice be? (r/p/sc/l/sp)")
     if humanChoice.lower()[0] == "s":
         if humanChoice.lower()[1] == "c":
             rps = "sc"
         elif humanChoice.lower()[1] == "p":
             rps = "sp"
+    #detecting if the answer is anything but scissors or spock
     if rps == "tehe this is just so the code doesn't break :)":
         rps = humanChoice.lower()[0]
     if rps in listOfChoice:
         print("Not a smart choice, but okay..")
     else:
         print("You didn't type rock, paper, scissors, lizard, or spock...")
-        choice()
+        continue
+    #getting the computers choice
     comChoice = random.choice(["r", "p", "sc", "l", "sp"])
+    #win, loss, or tie?
     if comChoice == rps:
         print("Its a tie!")
     elif comChoice in listOfWin[rps]:
@@ -30,10 +33,8 @@ def choice():
     else:
         print("Oh wow you lost LMAO couldn't be me")
     print("Your choice:", humanChoice,"\nComputers Choice:", comChoice)
-
+    #rematch?
     if input("Either way, would you like to try and murder me again?").lower()[0] == "y":
-         choice()
+         continue
     else:
         exit()
-
-choice()
